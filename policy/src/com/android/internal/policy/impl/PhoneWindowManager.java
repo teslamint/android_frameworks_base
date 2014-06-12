545c34997ba1ab4f3772f13a4229f7a2fd2b7d4c
Jaehoon You <teslamint@gmail.com>
2014-06-12 15:42:24 +0900

    add auto rotation, touch disabler button action
    Change-Id: I7caebdc76923b90716e160b92bfa58d85b97c5df

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -2259,6 +2259,24 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
         };
 
+  //TPT
+    void showToast(final String i) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+            CharSequence text = "Error";
+
+            text = i;
+
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(mContext, text, duration);
+            toast.show();
+            }
+        });
+    }
+  //TPT END
+
     /** {@inheritDoc} */
     @Override
     public long interceptKeyBeforeDispatching(WindowState win, KeyEvent event, int policyFlags) {
@@ -2611,7 +2629,39 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 return -1;
             }
-        }
+        } else if (keyCode == KeyEvent.KEYCODE_BRIGHTNESS_DOWN && down) { //TPT start
+            int ret = Settings.System.getInt(mContext.getContentResolver(), 
+                Settings.System.ACCELEROMETER_ROTATION, 0);
+
+            try {
+                if(ret == 0){
+                    mWindowManager.thawRotation();
+                    showToast("Auto Rotation Enabled");
+                } else {
+                    mWindowManager.freezeRotation(-1);
+                    showToast("Auto Rotation Disabled");
+                }
+            } catch (RemoteException exc) {
+                showToast("Auto Rotation Toggle Error");
+                Log.w(TAG, "Unable to save auto-rotate setting");
+            }
+        } else if (keyCode == KeyEvent.KEYCODE_BRIGHTNESS_UP && down) {
+            if(SystemProperties.getInt("touch.enable", 1) == 1) {
+                try {
+                    Runtime.getRuntime().exec("tpt-touch-enabler 0");
+                } catch(Exception d) {
+                    Log.w(TAG, "Unable to disable Touch: "+d.getMessage());
+                }
+                showToast("Touchscreen Disabled");
+            } else {
+                try {
+                    Runtime.getRuntime().exec("tpt-touch-enabler 1");
+                } catch(Exception d) {
+                    Log.w(TAG, "Unable to enable Touch: "+d.getMessage());
+                }
+                showToast("Touchscreen Enabled");
+            }
+        }//TPT End
 
         // Invoke shortcuts using Meta.
         if (down && repeatCount == 0 && !keyguardOn
